dbde46f58d0e6b0a8eedc46c19965914b9d49a0f
v.surtsov <v.surtsov@spacevm.org>
2025-10-07 16:54:50 +0300

    Fix build

--- devops/docker-entrypoint.sh
+++ devops/docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Fix permissions
 set -Eeuo pipefail
+
+rm -rf /var/www/html/ && mv /usr/src/wordpress/* /var/www/html && mv /usr/src/wordpress/.htaccess /var/www/html/.htaccess
+chown -R www-data:www-data /var/www/html/ && chmod 755 /var/www/html/
 
 exec "$@"
