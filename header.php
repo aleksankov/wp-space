47c51f9d5b091978fb1efd477e759fb4714271ac
Sergey Aleksankov <s.aleksankov@spacevm.org>
2026-01-30 14:24:54 +0000

    teamstorm fixes

--- header.php
+++ header.php
@@ -77,11 +77,20 @@
     <title><?= wp_get_document_title()?></title>
 </head>
 <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCHTT9K7"
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->
+        <?php wp_body_open(); ?>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+        ym(92150845, 'init', {webvisor:true, clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/92150845" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
     <header class="header<?= $page_submenu ? ' header--submenu' : ''; ?> compensate-for-scrollbar js-header">
         <div class="header__wrap">
             <div class="container">
